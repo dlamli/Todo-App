cc9595f241a9c329efa52bb0caff0d95142bafda
dlamli <dannylamli@hotmail.com>
2024-09-20 09:42:18 -0600

    [fix]Add production environment

--- src/main.tsx
+++ src/main.tsx
@@ -11,8 +11,17 @@ ReactDOM.createRoot(root).render(
         <KindeProvider
             clientId="f5fe70a21ae041058bfd80e72f932804"
             domain="https://dlamdev.kinde.com"
-            redirectUri="http://localhost:5173"
-            logoutUri="http://localhost:5173"
+            // redirectUri="http://localhost:5173"
+            redirectUri={
+                process.env.NODE_ENV === "production"
+                    ? "https://todo-app-dlam.netlify.app"
+                    : "http://localhost:5173"
+            }
+            logoutUri={
+                process.env.NODE_ENV === "production"
+                    ? "https://todo-app-dlam.netlify.app"
+                    : "http://localhost:5173"
+            }
         >
             <TodoProvider>
                 <TodoApp />
